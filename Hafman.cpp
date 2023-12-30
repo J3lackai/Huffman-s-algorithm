b319bc961bea319ed87a06d3d141de494ba7f0dd
Артём Матыцин <113178567+l3lackai@users.noreply.github.com>
2023-12-31 00:45:40 +0700

    Add files via upload

--- Hafman.cpp
+++ Hafman.cpp
@@ -18,6 +18,18 @@ struct Node
         left = right = nullptr;
     }
 };
+void DestroyTree(Node *node) // функция для освобождения памяти выделенной под дерево
+{
+    if (node)
+    {
+        if (node->left)
+            DestroyTree(node->left);
+        if (node->right)
+            DestroyTree(node->right);
+        delete node;
+    }
+    return;
+}
 // Сравнение двух узлов по частоте
 struct compare
 {
@@ -32,16 +44,14 @@ Node *buildHuffmanTree(string text)
     if (text.length() == 0)
     {
         cout << "Error: Text == ''";
-        exit;
+        exit(1);
     }
-
-    // Подсчет частоты встречаемости символов
-    unordered_map<char, int> freqCount; // создали хэш-таблицу для подсчёта слов <ключ, значение>
-    for (char c : text)                 // слева идентификатор для работы с элементом контейнера, справа контейнер.
+    unordered_map<char, int> freqCount;
+    // Подсчет частоты встречаемости символов в тексте
+    for (char c : text) // слева идентификатор для работы с элементом контейнера, справа контейнер.
     {
         freqCount[c]++;
     }
-
     priority_queue<Node *, vector<Node *>, compare> pq; //`vector` используется в качестве контейнера для хранения элементов приоритетной очереди.
     // он предоставляет динамический массив элементов, позволяя увеличивать и уменьшать его размер по мере необходимости.
     // compare позволяет определять приоритет при добавлении в очередь
@@ -80,12 +90,12 @@ Node *buildHuffmanTree(string text)
 void generateCodes(Node *root, string path, unordered_map<char, string> &codes) // codes - это хэш-таблица в которой ключ это символ, а значение его кодировка алгоритмом Хаффмана
 {
     if (root == nullptr) // случай когда дошли до листа дерева
-    {   
+    {
         return;
     }
     if (!root->left && !root->right)
     {
-        codes[root->data] = path;
+        codes[root->data] = path; // запоминаем путь в таблицу Хаффмана
     }
 
     generateCodes(root->left, path + "0", codes); // рекурсивно спускаемся по дереву от корня к листьям
@@ -106,49 +116,90 @@ string encode(string text, unordered_map<char, string> codes)
     return encodedText;
 }
 
-// Декодирование текста с помощью дерева Хаффмана
-string decode(Node *root, string encodedText)
+bool checkbool(string text)
 {
-    string decodedText = "";
-    Node *curr = root;
-
-    for (char c : encodedText)
+    int i = 0;
+    for (; i < text.length() && (text[i] == '0' || text[i] == '1'); i++)
+        ;
+    if (i == text.length())
+        return true;
+    return false;
+}
+// Декодирование текста с помощью дерева Хаффмана
+string decode(string encodedText, unordered_map<char, string> codes)
+{
+    if (!checkbool(encodedText))
     {
-        if (c == '0')
+        cout << "ERROR: the encodedText must contain only '1' or '0'";
+        exit(1);
+    }
+    string decodedText = "";
+    int i = 0;
+    int j = 0;
+    bool flag = true;
+    for (i = 0; i < encodedText.length() && flag;)
+    {
+        flag = false;
+        for (const auto &pair : codes)
         {
-            curr = curr->left;
-        }
-        else
-        {
-            curr = curr->right;
-        }
-
-        if (!curr->left && !curr->right)
-        {
-            decodedText += curr->data;
-            curr = root;
+            for (j = 0; j < pair.second.length() && encodedText[i + j] == pair.second[j]; j++)
+                ;
+            if (j == pair.second.length())
+            {
+                decodedText += pair.first;
+                i += j;
+                flag = true; // отслеживаем флаг чтобы избежать зацикливания при неверно введённых данных
+                break;
+            }
         }
     }
-
+    if (!flag)
+        cout << "ERROR: incorrect input data\n";
     return decodedText;
 }
-
 int main()
 {
+    unordered_map<char, string> codes; // таблица Хаффмана
+    Node *root = 0;
     string text = "";
+    bool flag = 0;
     cout << "Enter your text: ";
     cin >> text;
-    Node *root = buildHuffmanTree(text); // root - корень
-    unordered_map<char, string> codes;
-    generateCodes(root, "", codes);
-
-    string encodedText = encode(text, codes);
-    cout << "Encoded text: " << encodedText << endl;
-
-    string decodedText = decode(root, encodedText);
-    cout << "Decoded text: " << decodedText << endl;
-
-   
-    // DeleteTree(root); ОСВОБОЖДЕНИЕ ПАМЯТИ
+    cout << "Choose Decode or Encode (Enter 0 or 1): ";
+    cin >> flag;
+    if (flag)
+    {
+        root = buildHuffmanTree(text); // root - корень
+        generateCodes(root, "", codes);
+        string encodedText = encode(text, codes);
+        cout << "Encoded text: " << encodedText << endl;
+        DestroyTree(root);
+    }
+    else
+    {
+        if (!checkbool(text))
+        {
+            cout << "ERROR: the encodedText must contain only '1' or '0'";
+            return 1;
+        }
+        char sym;
+        string code = "";
+        int num = 0;
+        cout << "Enter num of different sym: ";
+        cin >> num;
+        if (num < 1)
+        {
+            cout << "ERROR: num of sym < 1";
+            return 1;
+        }
+        for (size_t i = 0; i < num; i++)
+        {
+            cout << "Enter the symbol and its code through backspace: "; // Ввод символов и их частот чтобы построить дерево для декодирования текста
+            cin >> sym >> code;
+            codes[sym] = code;
+        }
+        string decodedText = decode(text, codes);
+        cout << "Decoded text: " << decodedText << endl;
+    }
     return 0;
 }
